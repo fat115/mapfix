d739f87c5fe33467c26c43361a56495985d0c99e
fat115 <fat115@framasoft.org>
2017-08-04 11:10:21 +0200

    added intllib support added french translation

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
+-- Load support for intllib.
+local MP = minetest.get_modpath(minetest.get_current_modname())
+local S, NS = dofile(MP.."/intllib.lua")
+
 local function mapfix(minp, maxp)
 	local vm = minetest.get_voxel_manip(minp, maxp)
 	vm:update_liquids()
@@ -15,27 +19,27 @@ local delay = tonumber(minetest.setting_get("mapfix_delay")) or 15
 
 minetest.register_chatcommand("mapfix", {
 	params = "<size>",
-	description = "Recalculate the flowing liquids and the light of a chunk",
+	description = S("Recalculate the flowing liquids and the light of a chunk"),
 	func = function(name, param)
 		local pos = vector.round(minetest.get_player_by_name(name):getpos())
 		local size = tonumber(param) or default_size
 
 		if size >= 121 then
-			return false, "Radius is too big"
+			return false, S("Radius is too big")
 		end
 		local privs = minetest.check_player_privs(name, {server=true})
 		local time = os.time()
 
 		if not privs then
 			if size > max_size then
-				return false, "You need the server privilege to exceed the radius of " .. max_size .. " blocks"
+				return false, S("You need the server privilege to exceed the radius of @1 blocks", max_size)
 			elseif time - previous < delay then
-				return false, "Wait at least " .. delay .. " seconds from the previous \"/mapfix\"."
+				return false, S("Wait at least @1 seconds from the previous \"/mapfix\".", delay)
 			end
 			previous = time
 		end
 
-		minetest.log("action", name .. " uses mapfix at " .. minetest.pos_to_string(vector.round(pos)) .. " with radius " .. size)
+		minetest.log("action", S("@1 uses mapfix at @2 with radius @3", name, minetest.pos_to_string(vector.round(pos)), size))
 
 		size = math.max(math.floor(size - 8), 0) -- When passed to get_voxel_manip, positions are rounded up, to a multiple of 16 nodes in each direction. By subtracting 8 it's rounded to the nearest chunk border. max is used to avoid negative radius.
 
@@ -43,6 +47,6 @@ minetest.register_chatcommand("mapfix", {
 		local maxp = vector.add(pos, size)
 
 		mapfix(minp, maxp)
-		return true, "Done."
+		return true, S("Done.")
 	end,
 })
